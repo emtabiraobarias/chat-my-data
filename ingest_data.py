e98fe2dd1cbb48718c98e2bf66b2e9c2da0c5963
emtabiraobarias <emtabirao@gmail.com>
2024-04-05 15:55:33 +1100

    Copied working versions of the files from chat-your-data tutorial

---
+++ ingest_data.py
@@ -0,0 +1,24 @@
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import UnstructuredFileLoader
+from langchain_community.vectorstores.faiss import FAISS
+from langchain_openai import OpenAIEmbeddings
+
+print("Loading data...")
+loader = UnstructuredFileLoader("state_of_the_union.txt")
+raw_documents = loader.load()
+
+
+print("Splitting text...")
+text_splitter = CharacterTextSplitter(
+    separator="\n\n",
+    chunk_size=600,
+    chunk_overlap=100,
+    length_function=len,
+)
+documents = text_splitter.split_documents(raw_documents)
+
+
+print("Creating vectorstore...")
+embeddings = OpenAIEmbeddings()
+vectorstore = FAISS.from_documents(documents, embeddings)
+vectorstore.save_local("vectorstore")
